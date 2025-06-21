01559414748302e1b45a42af77a13cff16c964bc
Milan Yadav <ymilan593@gmail.com>
2025-06-21 22:54:54 +0545

    enhanced the UI and added css

--- main/forms.py
+++ main/forms.py
@@ -6,4 +6,9 @@ from .models import Todo
 class TodoForm(forms.ModelForm):
     class Meta:
         model = Todo
-        fields = ['title', 'description'] 
+        fields = ['title', 'description']
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea) 
