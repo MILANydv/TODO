9abaa681d1570827031e45fc8cb30f99c838b2d9
Milan Yadav <ymilan593@gmail.com>
2025-06-21 22:57:25 +0545

    completed the UI part

--- main/forms.py
+++ main/forms.py
@@ -6,15 +6,20 @@ from .models import Todo
 class TodoForm(forms.ModelForm):
     title = forms.CharField(
         max_length=200,
-        widget=forms.TextInput(attrs={'placeholder': 'Enter a new todo...'}),
+        widget=forms.TextInput(attrs={'placeholder': 'Todo title'}),
+        label=""
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Add a description...'}),
+        required=False,
         label=""
     )
 
     class Meta:
         model = Todo
-        fields = ['title']
+        fields = ['title', 'description']
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea) 
+    name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Your Name'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'Your Email'}))
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your Message'})) 
